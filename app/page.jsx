5f02e5a034421554e3ef0e85f4c4d6cf85001ed3
Jasim Uddin <jasimbdpro@gmail.com>
2025-02-08 19:10:25 +0600

    .

--- app/page.jsx
+++ app/page.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <section className=" w-full  flex-center flex-col" >
         <h1 className="head_text text-center">
-            Discover and Share
+            Discover and Share&nbsp;
             <br className="max-md:hidden" />
             <span className="orange_gradient text-center">AI-Powered Prompts</span>
             
