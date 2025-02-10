a59b728b9a109c088b3fb66bee299431e1348d70
Jasim Uddin <jasimbdpro@gmail.com>
2025-02-10 22:39:37 +0600

    .

--- models/user.js
+++ models/user.js
@@ -5,10 +5,13 @@ const UserSchema = newSchema({
         unique: [true, "Email already exist"],
         required: [true, "Email is required"]
     },
-    match: [
-        /^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/, 
-        "Username invalid, it should contain 8-20 alphanumeric characters and be unique!"
-      ]
+    username: {
+        type: String,
+        required: true,
+        match: [
+            /^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/,
+            "Username invalid, it should contain 8-20 alphanumeric characters and be unique!"
+        ]
     },
     image: {
         type: String,
