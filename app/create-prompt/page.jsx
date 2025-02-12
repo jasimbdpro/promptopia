115300e3e2546b8da66f30225d05b17e5170f0f0
Jasim Uddin <jasimbdpro@gmail.com>
2025-02-12 20:04:09 +0600

    .

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+"use client"
+import { useState } from 'react'
+import { useSession } from 'next-auth/react'
+import useRouter from 'next/navigation'
+
+import Form from '@components/Form'
 
 const CreatePrompt = () => {
+    const [setsubmitting, setSetsubmitting] = useState(false)
+    const [post, setPost] = useState({
+        prompt: ""
+        ,
+        tag: ""
+    })
+    const createPrompt = async (e) => {
+
+    }
+
     return (
-        <div>CreatePrompt</div>
+        <Form
+            type='Create'
+            post={post}
+            setPost={setPost}
+            setSetsubmitting={setSetsubmitting}
+            handleSubmit={createPrompt}
+        />
     )
 }
 
--- components/Form.jsx
+++ components/Form.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
-
-const Form = () => {
+import Link from 'next/link'
+const Form = ({ type, post, setPost, setSetsubmitting, handleSubmit,
+}) => {
   return (
-    <div>Form</div>
+    <section className=' w-full max-w-full flex-start flex-col'>
+      <h1 className=' head_text text-left'>{type}</h1>
+      <span className="blue_gradient">{type} Post</span>
+      <p className="desc text-left max-w-md">
+        {type} and share amazing prompt with the world, and let your imagination run wild with any AI powered platform.
+      </p>
+
+      <form action="" className=' mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism' onSubmit={handleSubmit}>
+        <label>
+          <span>Your AI Prompt</span>
+        </label>
+      </form>
+    </section>
   )
 }
 
