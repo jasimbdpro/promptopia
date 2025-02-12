19991bf8a6164670d9c244e2ab435b7ebf647b70
Jasim Uddin <jasimbdpro@gmail.com>
2025-02-12 21:03:56 +0600

    .

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -13,7 +13,20 @@ const CreatePrompt = () => {
         tag: ""
     })
     const createPrompt = async (e) => {
-
+        e.preventDefault()
+        setSubmitting(true)
+        try {
+            const response = await fetch('/api/prompt/new',
+                {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        prompt: post.prompt,
+                        userId: session.user.id,
+                    })
+                })
+        } catch (error) {
+            console.log(error)
+        }
     }
 
     return (
