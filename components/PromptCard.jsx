fc22f3badae21c738288a781f668a33376e5c1a2
Jasim Uddin <jasimbdpro@gmail.com>
2025-02-13 08:28:49 +0600

    .

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -4,7 +4,7 @@ import Image from "next/image"
 import { useSession } from "next-auth/react"
 import { usePathname, useRouter } from "next/navigation"
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
-
+  const [copied, setCopied] = useState("")
   if (!post || !post.creator) {
     return <div>Loading...</div> // Or any placeholder UI
   }
@@ -18,8 +18,27 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
             alt="User_Image"
             width={40}
             height={40}
-            className="rounded-full"
+            className="rounded-full object-contain"
           />
+          <div className="flex flex-col">
+            <h3 className="font-satoshi font-semibold text-gray-900">
+              {post.creator.username}
+            </h3>
+            <p className="font-inter text-sm text-gray-500">
+              {post.creator.email}
+            </p>
+          </div>
+        </div>
+        <div className="copy_btn" onClick={() => { }}>
+          <Image
+            src={copied === post.prompt
+              ? "/assets/icons/tick.svg"
+              : "/assets/icons/copy.svg"
+            }
+            width={12}
+            height={12}
+          />
+
         </div>
       </div>
     </div>
