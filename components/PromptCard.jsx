08d84f761903aaa158b0e3d9c4f5d2ae452129be
Jasim Uddin <jasimbdpro@gmail.com>
2025-02-13 09:23:40 +0600

    .

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -5,6 +5,13 @@ import { useSession } from "next-auth/react"
 import { usePathname, useRouter } from "next/navigation"
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
   const [copied, setCopied] = useState("")
+  const handleCopy = () => {
+    setCopied(post.prompt)
+    navigator.clipboard.writeText(post.prompt);
+    setTimeout(() => setCopied(""), 3000)
+
+  }
+
   if (!post || !post.creator) {
     return <div>Loading...</div> // Or any placeholder UI
   }
@@ -38,9 +45,14 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
             width={12}
             height={12}
           />
-
         </div>
       </div>
+      <p className="my-4 font-satoshi text-sm text-gray-700">{post.prompt}</p>
+      <p className=" font-inter text-sm blue_gradient cursor-pointer"
+        onClick={() => handleTagClick && handleTagClick(post.tag)}
+      >
+        {post.tag}
+      </p>
     </div>
   )
 }
