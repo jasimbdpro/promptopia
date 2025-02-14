2dbd0a1fed00244f0a98ea1ccdb64a6e36359317
Jasim Uddin <jasimbdpro@gmail.com>
2025-02-14 13:19:01 +0600

    .

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -27,28 +27,29 @@ const EditPrompt = () => {
         if (promptId) getPromptDetails()
     }, [promptId])
 
-    // const editPrompt = async (e) => {
-    //     e.preventDefault()
-    //     setSubmitting(true)
-    //     try {
-    //         const response = await fetch('/api/prompt/new',
-    //             {
-    //                 method: 'POST',
-    //                 body: JSON.stringify({
-    //                     prompt: post.prompt,
-    //                     userId: session?.user.id,
-    //                     tag: post.tag,
-    //                 })
-    //             })
-    //         if (response.ok) {
-    //             router.push('/')
-    //         }
-    //     } catch (error) {
-    //         console.log(error)
-    //     } finally {
-    //         setSubmitting(false)
-    //     }
-    // }
+    const updatePrompt = async (e) => {
+        e.preventDefault()
+        setSubmitting(true)
+
+        if (!promptId) alert("Prompt Id is not found")
+        try {
+            const response = await fetch(`/api/prompt/${promptId}`,
+                {
+                    method: 'PATCH',
+                    body: JSON.stringify({
+                        prompt: post.prompt,
+                        tag: post.tag,
+                    })
+                })
+            if (response.ok) {
+                router.push('/')
+            }
+        } catch (error) {
+            console.log(error)
+        } finally {
+            setSubmitting(false)
+        }
+    }
 
     return (
         <Form
@@ -57,7 +58,7 @@ const EditPrompt = () => {
             setPost={setPost}
             submitting={submitting}
             setSubmitting={setSubmitting}
-            handleSubmit={() => { }}
+            handleSubmit={updatePrompt}
         />
     )
 }
